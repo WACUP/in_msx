4da22e3c85c273a6627b553cf19817a1bf265f81
Darren Owen <winamp.plugins@gmail.com>
2025-07-23 08:36:00 +0100

    Avoid streamtitle & shorten the string checking

--- src/in_msx.cpp
+++ src/in_msx.cpp
@@ -218,10 +218,12 @@ extern "C" __declspec(dllexport) int winampGetExtendedFileInfoW(const wchar_t* f
         dest[1] = L'\0';
         return 1;
     }
-    else if (SameStrA(data, "streamgenre") ||
-             SameStrA(data, "streamtype") ||
-             SameStrA(data, "streamurl") ||
-             SameStrA(data, "streamname") ||
+    else if ((SameStrNA(data, "stream", 6) &&
+              (SameStrA((data + 6), "type") ||
+               SameStrA((data + 6), "genre") ||
+               SameStrA((data + 6), "url") ||
+               SameStrA((data + 6), "name") ||
+			   SameStrA((data + 6), "title"))) ||
              SameStrA(data, "reset"))
     {
         return 0;
